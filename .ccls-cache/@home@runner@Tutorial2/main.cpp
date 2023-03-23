85a69ce508e749ec782556ffdcf4eaaf3cb2ef90
IT22152732 <127375343+IT22152732@users.noreply.github.com>
2023-03-23 06:06:56 +0000

    exe4

--- .ccls-cache/@home@runner@Tutorial2/main.cpp
+++ .ccls-cache/@home@runner@Tutorial2/main.cpp
@@ -101,18 +101,28 @@ int volume(int height, int width, int length){
 using namespace std;
 
 void print(int len, int wth);
-void input(int len, int wth);
+void input(int &len, int &wth);
 // Do not change the main() function
 int main() {
  int length = 10, width = 5;
+  
  input(length, width);
  print(length, width);
  return 0;
 }
+
 // Do not change the print() function
 void print(int len, int wth) {
  cout << "Length : " << len
  << ", Width : " << wth << endl;
+}
+
+void input(int &len, int &wth){
+  cout<< "Enter Length :";
+  cin>> len;
+  cout<< "Enter Width : ";
+  cin>> wth;
+  cin>> wth;
 }
 
 
